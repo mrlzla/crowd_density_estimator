f5308632b54e29dd2297d40ed0a4b3841df5c4ea
mrlzla <mrlzlamogilev@gmail.com>
2017-05-23 16:48:13 +0300

    Add working version and keras callbacks

--- crowdnet.py
+++ crowdnet.py
@@ -1,91 +1,124 @@
-from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D, AveragePooling2D, Merge, BatchNormalization
-from keras.models import Sequential
+import os
+from keras.layers import Input, Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D, AveragePooling2D, Merge, BatchNormalization
+from keras.layers.merge import Concatenate
+from keras.callbacks import Callback, TensorBoard
+from keras.models import Model
 from preprocessing import load_data, preprocess_data
 
 
-def create_left_branch(model=Sequential(), kernel_size=(3, 3), input_shape=(225, 225, 1)):
-  model.add(Conv2D(64, kernel_size, input_shape=input_shape, padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(Conv2D(64, kernel_size, padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
-  model.add(Dropout(0.25))
+def create_left_branch(x, kernel_size=(3, 3)):
+  x = Conv2D(64, kernel_size, padding='same')(x)
+  x = BatchNormalization()(x)
+  x = Activation('relu')(x)
+  x = Conv2D(64, kernel_size, padding='same')(x)
+  x = BatchNormalization()(x)
+  x = Activation('relu')(x)
+  x = MaxPooling2D(pool_size=(2, 2), padding='same', strides=1)(x)
+  x = Dropout(0.25)(x)
 
-  model.add(Conv2D(128, kernel_size, padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(Conv2D(128, kernel_size, padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
-  model.add(Dropout(0.25))
+  x = Conv2D(128, kernel_size, padding='same')(x)
+  x = BatchNormalization()(x)
+  x = Activation('relu')(x)
+  x = Conv2D(128, kernel_size, padding='same')(x)
+  x = BatchNormalization()(x)
+  x = Activation('relu')(x)
+  x = MaxPooling2D(pool_size=(2, 2), padding='same', strides=1)(x)
+  x = Dropout(0.25)(x)
 
-  model.add(Conv2D(256, kernel_size, padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(Conv2D(256, kernel_size, padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(Conv2D(256, kernel_size, padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
-  model.add(Dropout(0.5))
+  x = Conv2D(256, kernel_size, padding='same')(x)
+  x = BatchNormalization()(x)
+  x = Activation('relu')(x)
+  x = Conv2D(256, kernel_size, padding='same')(x)
+  x = BatchNormalization()(x)
+  x = Activation('relu')(x)
+  x = Conv2D(256, kernel_size, padding='same')(x)
+  x = BatchNormalization()(x)
+  x = Activation('relu')(x)
+  x = MaxPooling2D(pool_size=(2, 2), padding='same', strides=1)(x)
+  x = Dropout(0.5)(x)
 
-  model.add(Conv2D(512, kernel_size, padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(Conv2D(512, kernel_size, padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(Conv2D(512, kernel_size, padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(3, 3), padding='same'))
-  model.add(Dropout(0.5))
+  #x = Conv2D(512, kernel_size, padding='same')(x)
+  #x = BatchNormalization()(x)
+  #x = Activation('relu')(x)
+  #x = Conv2D(512, kernel_size, padding='same')(x)
+  #x = BatchNormalization()(x)
+  #x = Activation('relu')(x)
+  #x = Conv2D(512, kernel_size, padding='same')(x)
+  #x = BatchNormalization()(x)
+  #x = Activation('relu')(x)
+  #x = MaxPooling2D(pool_size=(3, 3), padding='same', strides=1)(x)
+  #x = Dropout(0.5)(x)
 
-  model.add(Conv2D(512, kernel_size, padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(Conv2D(512, kernel_size, padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
-  model.add(Dropout(0.5))
+  x = Conv2D(512, kernel_size, padding='same')(x)
+  x = BatchNormalization()(x)
+  x = Activation('relu')(x)
+  x = Conv2D(512, kernel_size, padding='same')(x)
+  x = BatchNormalization()(x)
+  x = Activation('relu')(x)
+  x = MaxPooling2D(pool_size=(2, 2), padding='same', strides=1)(x)
+  x = Dropout(0.5)(x)
 
-  return model
+  return x
 
-def create_right_branch(model=Sequential(), kernel_size=(5, 5),  input_shape=(225, 225, 3)):
-  model.add(Conv2D(24, kernel_size, input_shape=input_shape, padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(AveragePooling2D(pool_size=(5, 5), padding='same'))
+def create_right_branch(x, kernel_size=(5, 5)):
+  x = Conv2D(24, kernel_size, padding='same')(x)
+  x = BatchNormalization()(x)
+  x = Activation('relu')(x)
+  x = AveragePooling2D(pool_size=(5, 5), padding='same', strides=1)(x)
 
-  model.add(Conv2D(24, kernel_size, padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(AveragePooling2D(pool_size=(5, 5), padding='same'))
+  x = Conv2D(24, kernel_size, padding='same')(x)
+  x = BatchNormalization()(x)
+  x = Activation('relu')(x)
+  x = AveragePooling2D(pool_size=(5, 5), padding='same', strides=1)(x)
 
-  model.add(Conv2D(24, kernel_size, padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(AveragePooling2D(pool_size=(5, 5), padding='SAME'))
+  x = Conv2D(24, kernel_size, padding='same')(x)
+  x = BatchNormalization()(x)
+  x = Activation('relu')(x)
+  x = AveragePooling2D(pool_size=(5, 5), padding='same', strides=1)(x)
 
-  return model
+  return x
 
-def create_model(model=Sequential(), input_shape=(225, 225, 3)):
-  left_branch = create_left_branch(input_shape=input_shape)
-  right_branch = create_right_branch(input_shape=input_shape)
+def create_model(batch_size=64, input_shape=(225, 225, 3)):
+  input_layer = Input(shape=input_shape)
+  left_branch = create_left_branch(input_layer)
+  right_branch = create_right_branch(input_layer)
 
-  model.add(Merge([left_branch, right_branch], mode='concat'))
-  model.add(Conv2D(1, (1, 1), padding='SAME'))
+  x = Concatenate()([left_branch, right_branch])
+  x = Conv2D(1, (1, 1), padding='same')(x)
+  model = Model(inputs=input_layer, outputs=x)
   model.compile(loss='mean_squared_error', optimizer='adam')
   return model
 
+class CheckPoints(Callback):
+  def on_train_begin(self, logs={}):
+    self.epoch_nmb = 0
+    return
+ 
+  def on_train_end(self, logs={}):
+    return
+
+  def on_epoch_begin(self, epoch, logs={}):
+    return
+
+  def on_epoch_end(self, epoch, logs={}):
+    path = "weights"
+    if not os.path.exists(path):
+        os.makedirs(path)
+    if self.epoch_nmb % 1000 == 0:
+      self.model.save("{}/step_{}.h5".format(path, self.epoch_nmb))
+    self.epoch_nmb += 1
+    return
+
+  def on_batch_begin(self, batch, logs={}):
+    return
+
+  def on_batch_end(self, batch, logs={}):
+    return
+
 if __name__ == '__main__':
-  im_generator = load_data()
-  model = create_model()
-  model.fit_generator(im_generator, epochs=100, verbose=True, steps_per_epoch=600)
+  batch_size=1
+  model = create_model(batch_size)
+  tensorboard_callback = TensorBoard(log_dir='./output', write_graph=True, write_images=True)
+  model.fit_generator(load_data(batch_size=batch_size), epochs=10000, verbose=True, steps_per_epoch=50,
+   callbacks=[CheckPoints(), tensorboard_callback])
   model.save('weights.h5')
--- preprocessing.py
+++ preprocessing.py
@@ -22,9 +22,6 @@ def get_density(filename, anno, imshape):
     with h5py.File(filename, 'r') as hf:
       density = np.array(hf.get('density'))
   else:
-    dirname = os.path.dirname("UCF_CC_50/densities")
-    if not os.path.exists(dirname):
-      os.makedir(dirname)
     density = create_density_map(anno, imshape)
     with h5py.File(filename, 'w') as hf:
       hf['density'] = density
@@ -65,7 +62,18 @@ def create_data(dirname='UCF_CC_50'):
       name = '.'.join(filename.split('.')[:-1])
       np_img = imread("{}/{}.jpg".format(dirname, os.path.splitext(filename)[0].split('_')[0]), mode='RGB')
       rows, columns, _= np_img.shape
-      density = get_density("UCF_CC_50/densities/{}.h5".format(name), anno, np_img.shape[:-1])
+
+      densities_path = "{}/densities".format(dirname)
+      if not os.path.exists(densities_path):
+        os.makedirs(densities_path)
+      results_path = "{}/results".format(dirname)
+      if not os.path.exists(results_path):
+        os.makedirs(results_path)
+      labels_path = "{}/labels".format(dirname)
+      if not os.path.exists(labels_path):
+        os.makedirs(labels_path)
+
+      density = get_density("{}/{}.h5".format(densities_path, name), anno, np_img.shape[:-1])
       for scale in np.arange(0.6, 1.3, 0.1):
         part_size = int(225 * scale)
         for i in range(0, rows - part_size, part_size/3):
@@ -73,33 +81,40 @@ def create_data(dirname='UCF_CC_50'):
               part_of_image = np_img[i:(i + part_size),j:(j + part_size)]
               part_of_density = density[i:(i + part_size),j:(j + part_size)]
               resized_image = imresize(part_of_image, (225, 225))
-              resized_density = imresize(part_of_density, (255, 255)) / scale / scale
+              resized_density = imresize(part_of_density, (225, 225)) / scale / scale
               cnt += 1
-              imsave("UCF_CC_50/results/{:0>5d}.jpg".format(cnt), resized_image)
-              with h5py.File("UCF_CC_50/labels/{:0>5d}.h5".format(cnt), 'w') as hf:
+              imsave("{}/{:0>5d}.jpg".format(results_path, cnt), resized_image)
+              with h5py.File("{}/{:0>5d}.h5".format(labels_path, cnt), 'w') as hf:
                 hf['density'] = resized_density
-      print(filename + "has already processed!")
+      print(filename + " has already processed!")
   #save_to_csv(results)
  
-def load_data(imagedir='UCF_CC_50/results', densitydir='UCF_CC_50/densities'):
+def load_data(batch_size = 64, imagedir='UCF_CC_50/results', densitydir='UCF_CC_50/labels'):
   filenames = get_filenames(dirname=imagedir, ext='jpg')
   np.random.shuffle(filenames)
-  for filename in filenames:
+  images, densities = np.zeros([batch_size, 225, 225, 3]), np.zeros([batch_size, 225, 225, 1])
+  #import ipdb; ipdb.set_trace()
+  for i, filename in enumerate(filenames):
     image = preprocess_data(imread("{}/{}".format(imagedir, filename)))
-    with h5py.File("{}/{}.h5".format(densitydir, int(filename.split('.')[0])), 'r') as hf:
+    with h5py.File("{}/{}.h5".format(densitydir, filename.split('.')[0]), 'r') as hf:
       density = np.array(hf.get('density'))
-
     if np.random.random() < 0.5:
       image = np.fliplr(image)
       density = np.fliplr(density)
-    yield image, density
+    density = np.expand_dims(density, 2)
+    images[i % batch_size] = image
+    densities[i % batch_size] = density
+    if (i + 1) % batch_size == 0:
+      yield np.array(images), np.array(densities)
+      images, densities = np.zeros([batch_size, 225, 225, 3]), np.zeros([batch_size, 225, 225, 1])
  
 def preprocess_data(image):
+  image = image.astype('float32')
   image[:,:,0] -= 103.939
   image[:,:,1] -= 116.779
   image[:,:,2] -= 123.68
   image /= 255.0
-  return np.expand_dims(image)
+  return image
 
 if __name__ == '__main__':
   create_data()
